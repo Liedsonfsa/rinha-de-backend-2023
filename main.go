f0e6cdeb8fe7ee5c976fb6573069d73482f9a4b5
Liedsonfsa <fabricoliedson@gmail.com>
2025-03-09 09:10:00 -0300

    feat: adding routes with gorilla mux

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"net/http"
 	"rinha-de-backend-2023/controllers"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	http.HandleFunc("POST /pessoas", controllers.InsertPeople)
-	http.HandleFunc("GET /pessoas/{id}", controllers.SearchByID)
-	http.HandleFunc("GET /pessoas", controllers.TermSearch)
-	http.HandleFunc("GET /contagem-pessoas", controllers.Count)
+	router := mux.NewRouter()
+	router.HandleFunc("/pessoas", controllers.InsertPeople).Methods("POST")
+	router.HandleFunc("/pessoas/{id}",controllers.SearchByID).Methods("GET")
+	router.HandleFunc("/pessoas", controllers.TermSearch).Methods("GET")
+	router.HandleFunc("/contagem-pessoas", controllers.Count).Methods("GET")
+
+	// http.HandleFunc("POST /pessoas", controllers.InsertPeople)
+	// http.HandleFunc("GET /pessoas/{id}", controllers.SearchByID)
+	// http.HandleFunc("GET /pessoas", controllers.TermSearch)
+	// http.HandleFunc("GET /contagem-pessoas", controllers.Count)
 	
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
+	// http.Handle("/", router)
 }
